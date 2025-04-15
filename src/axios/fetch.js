e09a28ebd012fc51146a435b484e1d8e833b92a9
macmini42 <macmini42@macmini-42.local>
2025-04-15 18:04:48 +0530

    Fetching data done.

--- src/axios/fetch.js
+++ src/axios/fetch.js
@@ -1,12 +1,12 @@
 import axios from "axios";
-import { useState } from "react";
+
 
 
 export const fetchUser = async() => {
     try{
         const response = await axios.get("https://67eb80a3aa794fb3222a7528.mockapi.io/users/test")
-        console.log(response.data)
-        return response.data
+        // console.log(response.data)
+        return response
         
     }
     catch(err){
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,18 +1,38 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { fetchUser } from '../axios/fetch'
 
 
 
 function ProductList() {
-
-    useEffect(() => {
-        fetchUser()
-
+  const [product, setProduct] = useState([])
+  const [search, setsearch] = useState('')
+  
+    useEffect(() => {const getData = async()=>{
+      try{
+        const result = await fetchUser()
+        setProduct(result.data)
+      }
+      catch(error){
+        console.log(error.message)
+      }
+    }      
+    getData()
     }, [])
+
+    console.log("data", product)
 
   return (
     <>
+      <input 
+      type='text'
+      onChange={(e) => setsearch(e.target.value)}
+      value={search}
+      placeholder='Search here...'
+      />
+      <ul>
+        {product.map((item) =>item.productName.toLowerCase().startsWith(search.toLowerCase()) && <li key={item.id}>{item.productName}</li>)}
+      </ul>
+
     </>
   )
 }
